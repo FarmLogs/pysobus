5b1c7ddf3b7f1660b00671a819fc8c03cb2aeffe
Michael Whalen <mwhalen@farmlogs.com>
2015-07-31 10:56:00 -0400

    Fixed some of setup.py for the message defs * Also cleaned up the README a tad * Also modified constants in parser.py to be more idomatic.

--- setup.py
+++ setup.py
@@ -1,17 +1,23 @@
 #!/usr/bin/env python
 
 import os
-from setuptools import setup
+import setuptools
+
 from pysobus import __version__
 
-setup(
+setuptools.setup(
     name='pysobus',
     version=__version__,
     description='Proprietary ISOBUS message specifications and decoding tools for yield data',
     author='Bryan Johnson',
     author_email='bryan@farmlogs.com',
     packages=['pysobus'],
-    data_files=[('pysobus', [os.path.join(os.path.dirname(os.path.realpath(__file__)), 'pysobus/message_definitions.csv')])],
+    package_dir={
+      'pysobus':'pysobus'
+    },
+    package_data={
+      'pysobus': ['message_definitions.csv']
+    },
     url='https://github.com/FarmLogs/pysobus',
     download_url='https://github.com/FarmLogs/pysobus/tarball/%s' % __version__,
     install_requires=['spanner>=0.3.4']
